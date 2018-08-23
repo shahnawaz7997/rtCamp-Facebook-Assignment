e052d6bda3dcd3357929a6619b633dc42774e370
Shahnawaz Memon <shahnawaz.memon07@gmail.com>
2018-08-23 15:04:26 +0530

    added siteConfig.php

---
+++ siteConfig.php
@@ -0,0 +1,76 @@
+<?php
+ini_set('max_execution_time', 300);
+
+$fb_app_id = '240715193433843';
+$fb_secret_id = 'cacf127ca28f009f996ee5a0888b095c';
+$fb_login_url = 'http://rtcamp-fb-assignment.000webhostapp.com/facebookProfile.php';
+
+require_once ('lib/Facebook/autoload.php');
+require_once ('lib/google/vendor/autoload.php');
+
+/**
+ *
+ * defines the namespace alies
+ * for the use of facebook namespace
+ * easy to use
+ */
+
+session_start();
+use Facebook\GraphObject;
+use Facebook\GraphSessionInfo;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookHttpable;
+use Facebook\HttpClients\FacebookCurl;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+
+// setting application configuration and session
+
+FacebookSession::setDefaultApplication($fb_app_id, $fb_secret_id);
+$helper = new FacebookRedirectLoginHelper($fb_login_url);
+
+if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
+	$session = new FacebookSession($_SESSION['fb_token']);
+	try {
+		if (!$session -> validate()) {
+			$session = null;
+		}
+	} catch ( Exception $e ) {
+		$session = null;
+	}
+}
+if (!isset($session) || $session === null) {
+	try {
+		$session = $helper -> getSessionFromRedirect();
+	} catch( FacebookRequestException $ex ) {
+		print_r($ex);
+	} catch( Exception $ex ) {
+		print_r($ex);
+	}
+}
+
+//It will fetch the data from facebook according to specified query passes as an argument
+function datafromfacebook($url){
+       $session = new FacebookSession($_SESSION['fb_token']);
+       $request = new FacebookRequest($session, 'GET', $url);
+	$response = $request -> execute();
+	$user = $response -> getGraphObject() -> asArray();
+
+	return $user;
+   }
+
+
+//Google Configration
+$google_redirect_url = "http://localhost/rtCampFBAssignment/googleAuth.php";
+$client = new Google_Client();
+$client->setAuthConfigFile('credentials.json');
+$client->setRedirectUri($google_redirect_url);
+$client->addScope(Google_Service_Drive::DRIVE);
+
+?>
