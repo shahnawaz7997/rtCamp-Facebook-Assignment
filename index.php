cd28e94b13eb469efc24edf38b1a84b2fb7c5f6c
Shahnawaz Memon <shahnawaz.memon07@gmail.com>
2018-08-25 16:14:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="">
         <link rel="apple-touch-icon" href="">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="node_modules/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="node_modules/css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     </head>
