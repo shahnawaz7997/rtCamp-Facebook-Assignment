a1b310911e4e940d3984baad7ab067f71e351c15
Shahnawaz Memon <shahnawaz.memon07@gmail.com>
2018-09-06 13:17:19 +0530

    now using lightGallery

--- albumSlider.php
+++ albumSlider.php
@@ -1,7 +1,4 @@
-<html>
-<head>
-	<title>Slideshow</title>
-	<?php
+<?php
 		use Facebook\FacebookSession;
 		use Facebook\FacebookRedirectLoginHelper;
 		use Facebook\FacebookRequest;
@@ -12,65 +9,22 @@
 		use Facebook\GraphObject;
 	
 		include('siteConfig.php');
-	?>
-	<script type="text/javascript" src="node_modules/js/jquery-2.2.3.min.js"></script>
-	<script type="text/javascript" src="node_modules/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="node_modules/js/bootstrap.js"></script>
-	<link rel="stylesheet" href="node_modules/css/bootstrap.css" />
-	<script type="text/javascript" src="node_modules/js/jquery.carousel.fullscreen.js"></script>
-</head>
-<body>
-	<div>
-		<a href="facebookProfile.php" style="position: fixed;top: 5px;left: 90%;z-index: 999;width: 150px;height: 23px;">
-		<span class="glyphicon glyphicon-remove " ></span></a>
-	</div>
-    <div id="carousel-generic" class="carousel slide" data-ride="carousel">
-    <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox" style="align-items: center;margin-left: 15% ;">
-        <?php
     	    if ( isset( $session ) )
 		    {
-			    $_SESSION['fb_login_session'] = $session;
-    			$_SESSION['fb_token'] = $session->getToken();
-    			$session = new FacebookSession( $session->getToken() );
-    			$alb_id=$_GET['ida'];
-    			$request_album_photo=new FacebookRequest($session,'GET','/'.$alb_id.'/photos?fields=source');
-    			$response_album_photo=$request_album_photo->execute();
-    			$album_photo=$response_album_photo->getGraphObject()->asArray();
+    			$alb_id=$_POST['album_id'];
+                $album_photo = datafromfacebook ( '/' . $alb_id . '/photos?fields=source&limit=2000&' );
     			if(!empty($album_photo))
     			{
-			        $f=0;
+                    $count=0;
 					foreach($album_photo['data'] as $alb)
 					{
 						$alb=(array) $alb;
-                        if($f==0)
-                        {
-                            echo '<div class="item active">';
-                            echo "<img class='img' src='".$alb['source']."'/>";
-                            echo '</div>';
-                            $f=1;
-                        }
-                        else
-                        {
-                            echo '<div class="item">';
-                            echo "<img class='img' src='".$alb['source']."'/>";
-                            echo '</div>';
-                        }
+
+                        $response[$count]['src'] = $alb['source'];
+                        $response[$count]['thumb'] = $alb['source'];
+                        $count++;
 					}
+                    echo json_encode($response);
     			}
 		    }
-        ?>
-        </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel-generic" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-  </body>
-</html>
+?>
